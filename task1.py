68e07bf0196bfc48ad168fda09e2540facc1cd0c
Oskar <80629512+GROMoOS@users.noreply.github.com>
2022-04-06 09:19:38 +0200

    cherry picked 4 commits

--- task1.py
+++ task1.py
@@ -1,2 +1,20 @@
 print("BananaEaters\n SCRUM Master SzymonGoldberg")
-print("SzymonGoldberg")
+print("Developer Vermilion")
+print("Developer Panzer0")
+print("GROMoOS")
+print("209424")
+
+
+def difference(a: float, b: float) -> float:
+    return a - b
+
+def multiply(A: float, B: float):
+    return A * B
+
+def Addition(A, B):
+    return A + B
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Illegal operation")
+    return a / b
